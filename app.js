51a0312464415754aaa1a7a896e073ef3f3dd4ed
suresh <64320966+codebrakerk@users.noreply.github.com>
2021-01-30 12:19:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 var preload =document.querySelector(".preloader");
-var btn = document.querySelector(".switch-btn");
-var video = document.querySelector(".video-container");
+//var btn = document.querySelector(".switch-btn");
+//var video = document.querySelector(".video-container");
 
 window.addEventListener("load",function(){
   preload.classList.add("hide-preloader");
 });
-
+/*
 btn.addEventListener("click",function(){
   if(!btn.classList.contains("slide")){
     btn.classList.add("slide");
@@ -19,3 +19,4 @@ btn.addEventListener("click",function(){
     video.play();
   }
 });
+*/
